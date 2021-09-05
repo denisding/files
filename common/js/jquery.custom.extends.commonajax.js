fd741bd5d833d7373daa2a674915ddbf5e29be1d
Denis Ding <yyguangzheng@163.com>
2021-09-05 10:41:38 +0800

    初始化提交文件

---
+++ common/js/jquery.custom.extends.commonajax.js
@@ -0,0 +1,26 @@
+﻿(function ($) {
+    $.extend({
+        CommonAjax: function (options, callBack) {
+            var defaults = {
+                url: '',
+                type: 'POST',
+                data: {},
+                async: true,
+                dataType: 'JSON',
+                success: function () { },
+                complete: function (xmlHttpRequest, status) {
+                    if (callBack) callBack();
+                    if (status === 'timeout') {
+                        layer.msg("超时错误！", { icon: 2, time: 3000 });
+                    }
+                },
+                error: function (jqXhr, textStatus, errorThrown) {
+                    if (callBack) callBack();
+                    layer.msg($.stringformat("出现错误！", errorThrown), { icon: 2, time: 3000 });
+                }
+            };
+            var opts = $.extend(defaults, options);
+            $.ajax(opts);
+        }
+    });
+})(jQuery);
---
+++ common/js/jquery.custom.extends.stringformat.js
@@ -0,0 +1,10 @@
+﻿(function ($) {
+	$.extend({
+		stringformat: function (string) {
+			var arg = arguments;
+			return string.replace(/{([\d])}/g, function (a, b) {
+				return arg[parseInt(b) + 1] || '';
+			});
+		}
+	});
+})(jQuery);
